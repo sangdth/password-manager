223041195b2ab5e44be564b636293d56b3fa5a09
Sang Dang <sangdth@macbookpro.local>
2017-09-13 16:00:03 +0300

    Fuck v-binb and move to submit form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,96 +1,18 @@
-Vue.component('password-list', {
+Vue.component('password-row', {
+  props: ['id', 'account', 'username', 'email', 'password', 'used', 'note'],
   template: `
-  <table class="table is-bordered is-fullwidth">
-    <thead>
       <tr>
-        <th>#</th>
-        <th>Account</th>
-        <th>Username</th>
-        <th>Email</th>
-        <th>Password</th>
-        <th>Note</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tfoot>
-      <tr>
-        <th>#</th>
-        <th>Account</th>
-        <th>Username</th>
-        <th>Email</th>
-        <th>Password</th>
-        <th>Note</th>
-        <th>Action</th>
-      </tr>
-    </tfoot>
-    <tbody>
-      <tr v-for="record in db">
-        <th>{{ record.id }}</th>
-        <td>{{ record.account }}</td>
-        <td>{{ record.username }}</td>
-        <td>{{ record.email }}</td>
-        <td>{{ record.password }}</td>
-        <td>{{ record.note }}</td>
+        <th>{{ id }}</th>
+        <td>{{ account }}</td>
+        <td>{{ username }}</td>
+        <td>{{ email }}</td>
+        <td>{{ password }} <span class="tag is-light is-pulled-right">{{ used }}</span></td>
+        <td>{{ note }}</td>
         <td>
-            <a class="button is-small is-outline tooltip" data-tooltip="Edit" data-placement="top" href="# "><i class="fa fa-pencil fa-lg "></i></a>
-            <a class="button is-small is-outline tooltip" data-tooltip="Delete" data-placement="top" href="# "><i class="fa fa-trash-o fa-lg "></i></a>
+          <a class="button is-small is-outline tooltip" data-tooltip="Edit" data-placement="top" href="# "><i class="fa fa-pencil fa-lg"></i></a>
+          <a class="button is-small is-outline tooltip" data-tooltip="Delete" data-placement="top" href="# "><i class="fa fa-trash-o fa-lg"></i></a>
         </td>
       </tr>
-    </tbody>
-  </table>
-  `,
-  data() {
-    return { 
-      db: [
-        {
-          "id": 1,
-          "account": "Google",
-          "username": "sangdth",
-          "email": "sangdth@gmail.com",
-          "password": "12345678",
-          "note": "lorem ipsum"
-        },
-        {
-          "id": 2,
-          "account": "Facebook",
-          "username": "sangdth",
-          "email": "sangdth@me.com",
-          "password": "12345678",
-          "note": "facebook ipsum"
-        },
-        {
-          "id": 3,
-          "account": "Twitter",
-          "username": "sangdth",
-          "email": "sangdth@me.com",
-          "password": "12345678",
-          "note": "twitter ipsum"
-        }
-    ]}
-  }
-});
-
-Vue.component('password-item', {
-  props: {
-    id: {
-      type: Number,
-      required: true
-    },
-    account: String,
-    username: String,
-    email: String,
-    password: String,
-    note: String
-  },
-  template: `
-    <tr>
-      <th>{{ id }}</th>
-      <td>{{ account }}</td>
-      <td>{{ username }}</td>
-      <td>{{ email }}</td>
-      <td>{{ password }}</td>
-      <td>{{ note }}</td>
-    </tr>
   `
 });
 
@@ -99,6 +21,44 @@ new Vue({
 
   data: {
     msg: 'Hi, search your passwords here',
-  }
+    database: [
+      {
+        id: 1,
+        account: 'Google',
+        username: 'sangdth',
+        email: 'sangdth@gmail.com',
+        password: '****',
+        used: 3214,
+        note: ''
+      },
+      {
+        id: 2,
+        account: 'Facebook',
+        username: 'sangdth',
+        email: 'sangdth@me.com',
+        password: '****',
+        used: 762,
+        note: 'facebook ipsum'
+      },
+      {
+        id: 3,
+        account: 'Twitter',
+        username: 'sangdth',
+        email: 'sangdth@me.com',
+        password: '****',
+        used: 14,
+        note: 'twitter ipsum'
+      },
+      {
+        id: 4,
+        account: 'Github',
+        username: 'sangdth',
+        email: 'sangdth@gmail.com',
+        password: '****',
+        used: 456,
+        note: 'github ipsum'
+      }
+    ],
+  },
 
 });
