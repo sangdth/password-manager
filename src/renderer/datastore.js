4dfedfbb2f1f8b9562f0761613c4aeb0dd4dd4d4
Sang Dang <sangdth@gmail.com>
2017-09-28 17:01:13 +0300

    Test encrypt function

--- src/renderer/datastore.js
+++ src/renderer/datastore.js
@@ -5,4 +5,32 @@ import { remote } from 'electron';
 export default new Datastore({
   autoload: true,
   filename: path.join(remote.app.getPath('userData'), '/data.db'),
+  afterSerialization: function (doc) {
+    // const msg = doc.split('').reverse().join('');
+    
+		console.log('this is afterSerialization with ' + doc );
+    return doc;
+		// var encrypted = doc;
+		
+		// try{
+		// 	var ef;
+		// 	if(JSON.parse(doc) && isNaN(doc)){
+		// 		var cipher = crypto.createCipher(algorithm, key);
+		// 		encrypted=cipher.update(JSON.stringify(doc), 'utf8', 'hex') + cipher.final('hex');
+		// 		ef = encrypted;
+		// 		console.log("Ser" +encrypted);
+		// 		return encrypted;
+		// 		}
+		// }catch(e){
+		// 		
+		// }
+		// da=encrypted;
+		
+	},
+  beforeDeserialization: function(doc) {
+    // const msg = doc.split('').reverse().join('');
+    
+		console.log('this is beforeDeserialization with ' + doc );
+    return doc;
+  }
 });
